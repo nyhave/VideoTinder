9a6ee7f03c350d3f60282427d9016d062822e004
nyhave <nyhave@gmail.com>
2025-07-26 16:47:09 +0200

    Add Realetten group call placeholder

--- src/firebase.js
+++ src/firebase.js
@@ -14,6 +14,7 @@ import {
   setDoc,
   addDoc,
   arrayUnion,
+  arrayRemove,
   increment
 } from 'firebase/firestore';
 import {
@@ -307,6 +308,7 @@ export {
   setDoc,
   addDoc,
   arrayUnion,
+  arrayRemove,
   increment,
   ref,
   uploadBytes,
