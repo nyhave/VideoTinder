e55b44354fe34c446b1ee36ec15e1268e52332d8
nyhave <nyhave@gmail.com>
2025-07-27 08:25:37 +0200

    feat: improve load more logic

--- src/components/DailyDiscovery.jsx
+++ src/components/DailyDiscovery.jsx
@@ -326,10 +326,8 @@ export default function DailyDiscovery({ userId, onSelectProfile, ageRange, onOp
     React.createElement(Button, {
       className: 'mt-4 w-full bg-yellow-500 text-white',
       onClick: () => {
-        const hasSub = hasActiveSub(user);
-        const extra = user.extraClipsDate === today ? 3 : 0;
-        const limit = (hasSub ? 6 : 3) + extra;
-        if (filtered.length < limit) {
+        const moreCandidates = scored.length > filtered.length;
+        if (!moreCandidates) {
           setShowExtend(true);
         } else if (user.extraClipsDate === today) {
           setShowInfo(true);
