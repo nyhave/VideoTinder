054b0fd63238a8c4d01ae2750a46ed346abab42e
nyhave <nyhave@gmail.com>
2025-07-27 16:59:33 +0200

    Show game for all participants

--- src/components/RealettenGameOverlay.jsx
+++ src/components/RealettenGameOverlay.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import TurnGame from './TurnGame.jsx';
 import { X } from 'lucide-react';
 
-export default function RealettenGameOverlay({ players, onClose }) {
+export default function RealettenGameOverlay({ players, myName, onClose }) {
   return React.createElement('div', { className:'fixed inset-0 z-50 bg-black/70 flex items-center justify-center' },
     React.createElement('div', { className:'relative w-full max-w-2xl mx-4' },
-      React.createElement(TurnGame, { players, onExit:onClose }),
+      React.createElement(TurnGame, { players, myName, onExit:onClose }),
       React.createElement('button', { className:'absolute top-2 right-2 text-white bg-black/40 rounded-full p-1', onClick:onClose },
         React.createElement(X, { className:'w-6 h-6' })
       )
