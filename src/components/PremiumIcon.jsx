918dbd67bf1921df12c7b8b432051b166728f58b
nyhave <nyhave@gmail.com>
2025-08-06 07:24:41 +0200

    Add reusable premium icon and integrate

---
+++ src/components/PremiumIcon.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Crown } from 'lucide-react';
+
+export default function PremiumIcon({ className = 'w-5 h-5 text-yellow-500' }) {
+  return React.createElement(Crown, { className });
+}
